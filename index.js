c8b2552d2ec6c6a2f21f8d4bd5ee4de9dd310b76
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2026-01-10 22:40:44 +0100

    delete html not used

--- index.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('header');
-
- window.addEventListener('scroll', () => {
-     if (window.scrollY > 50) {
-         header.style.backgroundColor = 'rgba(45, 45, 45, 0.365)';
-     } else {
-         header.style.backgroundColor = 'rgba(45, 45, 45, 1)';
-     }
- });
